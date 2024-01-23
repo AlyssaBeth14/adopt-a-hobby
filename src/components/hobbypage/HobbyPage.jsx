79072cb3bc703b72b15915b6da1c4cf860b59218
Alyssa Burdett <alyssa.burdett14@gmail.com>
2024-01-23 14:07:17 -0700

    added new branch

--- src/components/hobbypage/HobbyPage.jsx
+++ src/components/hobbypage/HobbyPage.jsx
@@ -1,9 +1,11 @@
+import React from 'react'
+
 const HobbyPage = () => {
-    return (
-        <div>
-            <h1>This is the hobby page</h1>
-        </div>
-    )
+  return (
+    <div>
+        <h1>This is the hobby page</h1>
+    </div>
+  )
 }
 
 export default HobbyPage
