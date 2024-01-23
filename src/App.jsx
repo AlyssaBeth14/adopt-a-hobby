e79071517d52d6578acd82737e3d6eda9742d635
SeanChid <seanchidester737@gmail.com>
2024-01-22 16:06:44 -0800

    added page routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Homepage from './components/homepage/Homepage'
+import HobbyPage from './components/hobbypage/HobbyPage'
+import CYOPage from './components/cyopage/CYOPage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/hobby' element={<HobbyPage />} />
+        <Route path='/cyo-hobby' element={<CYOPage />} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/components/cyopage/CYOPage.jsx
@@ -0,0 +1,9 @@
+const CYOPage = () => {
+    return (
+        <div>
+            <h1>This is the CYO page</h1>
+        </div>
+    )
+}
+
+export default CYOPage
