a7ae471c3700476350e32146d876b54d37a2f52a
SeanChid <seanchidester737@gmail.com>
2024-01-23 12:16:46 -0800

    added backend routes and server setup

---
+++ server/controller.js
@@ -0,0 +1,133 @@
+import { Hobby, Supply, Tutorial, Admin } from './db/models.js'
+
+const handlerFunctions = {
+
+    getHobbies: async (req, res) => {
+        const hobbies = await Hobby.findAll()
+
+        res.send(hobbies)
+    },
+
+    getHobby: async (req, res) => {
+        const {hobbyId} = req.params
+
+        const hobby = await Hobby.findByPk(hobbyId)
+        
+        res.send(hobby)
+    },
+    
+    getSupply: async (req, res) => {
+        // gets all supplies for a specified hobby ID and if optional
+        const {hobbyId, optional} = req.query
+        
+        const query = {}
+        if (hobbyId) {
+            query.hobbyId = hobbyId
+        }
+        if (optional) {
+            query.optional = optional
+        }
+        const supplies = await Supply.findAll({
+            where: query
+        })
+
+        res.send(supplies)
+    },
+
+    getTutorials: async (req, res) => {
+        // gets all tutorials specified by hobby ID and by paid
+        const {hobbyId, paid} = req.query
+
+        const query = {}
+        if (hobbyId) {
+            query.hobbyId = hobbyId
+        }
+        if (paid) {
+            query.paid = paid
+        }
+        const tutorials = await Tutorial.findAll({
+            where: query
+        })
+
+        res.send(tutorials)
+    },
+
+    addHobby: async (req, res) => {
+        const {hobbyName, hobbyImg, category} = req.body
+
+        const newHobby = {
+            hobbyName,
+            hobbyImg,
+            category
+        }
+
+        await Hobby.create(newHobby)
+        const hobbies = await Hobby.findAll()
+
+        res.send(hobbies)
+    },
+    
+    addSupply: async (req, res) => {
+        const {hobbyId, supplyName, optional} = req.body
+
+        const newSupply = {
+            hobbyId,
+            supplyName,
+            optional
+        }
+
+        await Supply.create(newSupply)
+        const supplies = await Supply.findAll()
+
+        res.send(supplies)
+    },
+
+    addTutorial: async (req, res) => {
+        const {hobbyId, tutorialImg, tutorialName, tutorialLink, paid} = req.body
+
+        const newTutorial = {
+            hobbyId,
+            tutorialImg,
+            tutorialName,
+            tutorialLink,
+            paid
+        }
+
+        await Tutorial.create(newTutorial)
+        const tutorials = await Tutorial.findAll()
+
+        res.send(tutorials)
+    },
+    
+    deleteHobby: async (req, res) => {
+        const {hobbyId} = req.params
+
+        const hobby = await Hobby.findByPk(hobbyId)
+        await hobby.destroy()
+        const hobbies = await Hobby.findAll()
+
+        res.send(hobbies)
+    },
+
+    deleteSupply: async (req, res) => {
+        const {supplyId} = req.params
+
+        const supply = await Supply.findByPk(supplyId)
+        await supply.destroy()
+        const supplies = await Supply.findAll()
+
+        res.send(supplies)
+    },
+
+    deleteTutorial: async (req, res) => {
+        const {tutorialId} = req.params
+
+        const tutorial = await Tutorial.findByPk(tutorialId)
+        await tutorial.destroy()
+        const tutorials = await Tutorial.findAll()
+
+        res.send(tutorials)
+    }
+}
+
+export default handlerFunctions
---
+++ server/index.js
@@ -0,0 +1,36 @@
+import express from 'express'
+import ViteExpress from 'vite-express'
+
+const app = express()
+
+app.use(express.json())
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: false}))
+
+import handlerFunctions from './controller.js'
+const {
+    getHobbies,
+    getHobby,
+    getSupply,
+    getTutorials,
+    addHobby,
+    addSupply,
+    addTutorial,
+    deleteHobby,
+    deleteSupply,
+    deleteTutorial
+
+} = handlerFunctions
+
+app.get('/hobbies', getHobbies)
+app.get('/hobby/:hobbyId', getHobby)
+app.get('/supply', getSupply)
+app.get('/tutorials', getTutorials)
+app.post('/hobby', addHobby)
+app.post('/supply', addSupply)
+app.post('/tutorial', addTutorial)
+app.delete('/hobby/:hobbyId', deleteHobby)
+app.delete('/supply/:supplyId', deleteSupply)
+app.delete('/tutorial/:tutorialId', deleteTutorial)
+
+ViteExpress.listen(app, 8000, () => console.log('server is running on 8000'))
