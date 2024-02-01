be2c7e4dee0852f04a6f07c3c3b86ade5e814e0d
SeanChid <seanchidester737@gmail.com>
2024-02-01 15:16:01 -0800

    fixed tutorialsCard

--- src/components/admin/tutorialsCard.jsx
+++ src/components/admin/tutorialsCard.jsx
@@ -5,42 +5,43 @@ const TutorialsCard = (props) => {
 
   const [paid, setPaid] = useState(props.paid)
 
-// Delete supply function
-const deleteSupply = () => {
-  const confirmDelete = window.confirm('Sure want to delete supply?')
-  if (confirmDelete) {
+  // Delete supply function
+  const deleteSupply = () => {
+    const confirmDelete = window.confirm('Sure want to delete supply?')
+    if (confirmDelete) {
 
-    axios.delete(`/api/tutorial/${props.tutroialId}?hobbyId=${props.hobbyId}`)
-      .then((res) => {
-        props.setTutorials(res.data)
-        console.log(res.data);
-      })
-      .catch((error) => {
-        console.error('Error deleting supply:', error);
-      })
+      axios.delete(`/api/tutorial/${props.tutroialId}?hobbyId=${props.hobbyId}`)
+        .then((res) => {
+          props.setTutorials(res.data)
+          console.log(res.data);
+        })
+        .catch((error) => {
+          console.error('Error deleting supply:', error);
+        })
+    }
   }
 
-const handleChecked = () => {
-  axios.put(`/api/tutorial/${props.tutorialId}`, { paid: !paid })
-    .then((res) => {
-      const newTutorials = [...props.tutorials]
-      for (let tutorial of newTutorials) {
-        if (tutorial.tutorialId === res.data.tutorialId) {
-          tutorial.paid = res.data.paid
-          break
+  const handleChecked = () => {
+    axios.put(`/api/tutorial/${props.tutorialId}`, { paid: !paid })
+      .then((res) => {
+        const newTutorials = [...props.tutorials]
+        for (let tutorial of newTutorials) {
+          if (tutorial.tutorialId === res.data.tutorialId) {
+            tutorial.paid = res.data.paid
+            break
+          }
         }
-      }
-      props.setTutorials(newTutorials)
-      setPaid(!paid)
-    })
-}
+        props.setTutorials(newTutorials)
+        setPaid(!paid)
+      })
+  }
 
   return (
     <>
       <div>
         {props.tutorialName}
         {paid ? ": payment required" : ""}
-        {props.isEditing && <input type='checkbox' value={paid} checked={paid} onChange={handleChecked}/>}
+        {props.isEditing && <input type='checkbox' value={paid} checked={paid} onChange={handleChecked} />}
         {props.isEditing && <button className="D-button" onClick={deleteSupply}>Delete</button>}
       </div>
     </>
