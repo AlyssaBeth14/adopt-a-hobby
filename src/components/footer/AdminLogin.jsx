aa8ac2449e3b51ea2e08d1c410961bb13cb2971b
SeanChid <seanchidester737@gmail.com>
2024-02-07 15:06:56 -0800

    added event modals

--- src/components/footer/AdminLogin.jsx
+++ src/components/footer/AdminLogin.jsx
@@ -5,12 +5,14 @@ import { useNavigate } from 'react-router-dom'
 
 const AdminLogin = (props) => {
 
-    const {showModal, setShowModal} = props
+    const { showModal, setShowModal } = props
+    const [showErrorModal, setShowErrorModal] = useState(false)
+    const [errorMessage, setErrorMessage] = useState('')
     const [adminName, setAdminName] = useState('')
     const [adminPass, setAdminPass] = useState('')
     const navigate = useNavigate()
 
-    
+
     const handleSubmit = (e) => {
         e.preventDefault()
 
@@ -20,56 +22,69 @@ const AdminLogin = (props) => {
         }
 
         axios.post('/api/admin', adminObject)
-        .then((res) => {
-            if (res.status === 200) {
-                navigate('/admin')
-                setShowModal(false)
-            }
-        })
-        .catch((error) => {
-            if (error.response) {
-                if (error.response.status === 401) {
-                    alert('Invalid admin credentials')
-                } else if (error.response.status === 404) {
-                    alert('Admin not found')
+            .then((res) => {
+                if (res.status === 200) {
+                    navigate('/admin')
+                    setShowModal(false)
                 }
-            } else {
-                console.log(error)
-            }
-        })
+            })
+            .catch((error) => {
+                if (error.response) {
+                    if (error.response.status === 401) {
+                        setErrorMessage('Invalid admin credentials')
+                        setShowErrorModal(true)
+                    } else if (error.response.status === 404) {
+                        setErrorMessage('Admin not found')
+                        setShowErrorModal(true)
+                    }
+                } else {
+                    console.log(error)
+                }
+            })
     }
 
     return (
-        <Modal show={showModal} onHide={() => setShowModal(false)}>
-            <Modal.Header closeButton className='modalHeader'>
-                <Modal.Title>Admin Login</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <form onSubmit={handleSubmit}>
-                    <label className='form-label'>
-                        Username
-                        <input
-                            type='text'
-                            className='form-control'
-                            value={adminName}
-                            onChange={(e) => setAdminName(e.target.value)}
-                        />
-                    </label>
-                    <br/>
-                    <label className='form-label'>
-                        Password
-                        <input
-                            type='password'
-                            className='form-control'
-                            value={adminPass}
-                            onChange={(e) => setAdminPass(e.target.value)}
-                        />
-                    </label>
-                    <br/>
-                    <button type='submit' className='btn yellowButton'>Login</button>
-                </form>
-            </Modal.Body>
-        </Modal>
+        <div>
+            <Modal show={showModal} onHide={() => setShowModal(false)}>
+                <Modal.Header closeButton className='modalHeader'>
+                    <Modal.Title>Admin Login</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <form onSubmit={handleSubmit}>
+                        <label className='form-label'>
+                            Username
+                            <input
+                                type='text'
+                                className='form-control'
+                                value={adminName}
+                                onChange={(e) => setAdminName(e.target.value)}
+                            />
+                        </label>
+                        <br />
+                        <label className='form-label'>
+                            Password
+                            <input
+                                type='password'
+                                className='form-control'
+                                value={adminPass}
+                                onChange={(e) => setAdminPass(e.target.value)}
+                            />
+                        </label>
+                        <br />
+                        <button type='submit' className='btn yellowButton'>Login</button>
+                    </form>
+                </Modal.Body>
+            </Modal>
+
+            <Modal show={showErrorModal} onHide={() => setShowErrorModal(false)}>
+                <Modal.Header closeButton className='modalHeader'>
+                    <Modal.Title>Error</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <h4>{errorMessage}</h4>
+                </Modal.Body>
+            </Modal>
+        </div>
     )
 }
 
